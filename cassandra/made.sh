f78a1d2a60f7ec50d094be9896361451f4acda9c
Mario Gray <mario.gray@gmail.com>
2023-05-11 18:47:49 -0700

    rever to originals

--- cassandra/made.sh
+++ cassandra/made.sh
@@ -3,3 +3,4 @@ go mod tidy
 docker build --no-cache --progress=plain -f build-golang.docker -t build-golang .
 docker run --name go-builder docker.io/library/build-golang
 docker cp go-builder:/app/image/files/cassandra-seed.so ./image/files
+#kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.24/deploy/local-path-storage.yaml
